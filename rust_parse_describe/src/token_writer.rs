9101ff279abde04480d7776c5d12e31f7c11a1d6
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2015-12-07 13:30:42 +0000

    Correct writeStringToken

--- rust_parse_describe/src/token_writer.rs
+++ rust_parse_describe/src/token_writer.rs
@@ -2,29 +2,41 @@ use ::core_util::*;
 
 use std::result;
 
-pub fn writeStringToken<ERR> (string : &str, out : &mut CharOutput<ERR>) -> result::Result<(), ERR> {
+pub fn writeStringToken<ERR>(string : &str, out : &mut CharOutput<ERR>) -> result::Result<(), ERR> {
 	
 	use std::fmt::Write;
 	
+	try!(out.write_char('"'));
+	
 	for ch in string.chars() {
 		
+		if ch == '"' || ch == '\\' {
+			try!(out.write_char('\\'));
+		}
 		try!(out.write_char(ch));
 		
 	}
 	
+	try!(out.write_char('"'));
+	
 	Ok(())
 }
 
-#[cfg(test)]
+pub fn writeStringToken_toString(string : &str) -> String {
+	let mut result = String::new();
+	writeStringToken(string, &mut result as &mut CharOutput<()>).unwrap();
+	result
+}
+
+#[test]
 fn test_writeStringToken() {
 	
-	fn check_writeStringToken(string : &str, expected : &str) {
-		let mut result = String::new();
-		writeStringToken(string, &mut result as &mut CharOutput<()>);
-		assert_eq!(result, expected);
-	}
+	assert_eq!(writeStringToken_toString(""), r#""""#);
+	assert_eq!(writeStringToken_toString("abc"), r#""abc""#);
+	assert_eq!(writeStringToken_toString(r#"-"-"#), r#""-\"-""#);
+	assert_eq!(writeStringToken_toString(r#"""#), r#""\"""#);
+	assert_eq!(writeStringToken_toString(r#"\"#), r#""\\""#);
+	assert_eq!(writeStringToken_toString(r#"--\"-"#), r#""--\\\"-""#);
+	assert_eq!(writeStringToken_toString(r#"---\"#), r#""---\\""#);
 	
-	check_writeStringToken("abc", r#""abc""#);
-	
-	check_writeStringToken("a\"bc\"", r#""a\"bc\"""#);
 }
